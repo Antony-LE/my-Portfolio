33637ea166394a551aa4cc113ff65d59bae853e6
Antony-LE <le.antony.l@gmail.com>
2022-04-20 14:07:59 +0200

    Add spaceX project

--- src/data/data-projects.js
+++ src/data/data-projects.js
@@ -1,17 +1,25 @@
 const projectsData = [
   {
     title: 'Running Wild',
-    subtitle: 'React, NodeJS, PostgreSQL',
+    subtitle: 'React, NodeJS, PostgreSQL, API',
     description: 'Projet de fin d\'étude centré sur la gamification de la course à pieds ',
     link: 'https://running-wild-e6097.web.app/',
     image: 'projet-runningwild.PNG',
   },
   {
     title: 'Fooder',
-    subtitle: 'React, NodeJS',
+    subtitle: 'React, NodeJS, API, homemade CSS',
     description: 'Quand on ne sait pas quoi cuisiner, le site Fooder est la solution ! ',
     link: 'https://fooder-ce9c0.web.app/',
     image: 'projet-fooder.PNG',
+  },
+
+  {
+    title: 'SpaceX-launches',
+    subtitle: 'VueJS, Vuetify, NodeJS, API',
+    description: ' Fan de spaceX ? Devenez incollable sur leurs lancements! ',
+    link: 'https://spacex-launches-22fbf.web.app/',
+    image: 'projet-spaceX-launches.PNG',
   },
 ];
 export default projectsData;
