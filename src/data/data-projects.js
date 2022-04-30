051137f93ca7ec03c4f029e692732b28524e796a
Antony-LE <le.antony.l@gmail.com>
2022-04-30 09:09:19 +0200

    Add firebase skill on projects

--- src/data/data-projects.js
+++ src/data/data-projects.js
@@ -1,14 +1,14 @@
 const projectsData = [
   {
     title: 'Running Wild',
-    subtitle: 'React, NodeJS, PostgreSQL, API',
+    subtitle: 'React, NodeJS, PostgreSQL, API, Firebase',
     description: 'Projet de fin d\'étude centré sur la gamification de la course à pieds ',
     link: 'https://running-wild-e6097.web.app/',
     image: 'projet-runningwild.PNG',
   },
   {
     title: 'Fooder',
-    subtitle: 'React, NodeJS, API, homemade CSS',
+    subtitle: 'React, NodeJS, API, homemade CSS, Firebase',
     description: 'Quand on ne sait pas quoi cuisiner, le site Fooder est la solution ! ',
     link: 'https://fooder-ce9c0.web.app/',
     image: 'projet-fooder.PNG',
@@ -16,7 +16,7 @@ const projectsData = [
 
   {
     title: 'SpaceX-launches',
-    subtitle: 'VueJS, Vuetify, NodeJS, API',
+    subtitle: 'VueJS, Vuetify, NodeJS, API, Firebase',
     description: ' Fan de spaceX ? Devenez incollable sur leurs lancements! ',
     link: 'https://spacex-launches-22fbf.web.app/',
     image: 'projet-spaceX-launches.PNG',
